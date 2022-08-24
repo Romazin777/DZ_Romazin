3d5466a6505fd86936dd1239d56778cb7dd84270
Denis Romazin <romazin777@gmail.com>
2022-08-24 22:28:03 +0500

    Задача №13

---
+++ quest_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите целое число");
+int NumberAll = Convert.ToInt32(Console.ReadLine());
+if ((99 / NumberAll) >= 1)
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+}
+else
+{
+    string number = NumberAll.ToString();
+    int three = int.Parse(number[2].ToString());
+    Console.Write("Третья цифра введенного числа: " + three);
+}
